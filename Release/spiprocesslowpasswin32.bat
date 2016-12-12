6c54e8d2a04ea32b259c8d863ab8ad396c4aaa71
Stephane Poirier <stephane.poirier@oifii.org>
2016-12-12 08:55:16 -0500

    first commit

---
+++ Release/spiprocesslowpasswin32.bat
@@ -0,0 +1,4 @@
+rem start spiprocesslowpasswin32.exe "E-MU ASIO" 0 1 "E-MU ASIO" 2 3 100.0 -1 1 1 900 1000 220 1 0 0 24 255 200 0 "mywindowclass" "mywindowtitle" "" ""
+rem start spiprocesslowpasswin32.exe "E-MU ASIO" 4 5 "E-MU ASIO" 2 3 100.0 -1 1 1 900 1000 220 1 0 0 24 255 200 0 "mywindowclass" "mywindowtitle" "" ""
+rem start spiprocesslowpasswin32.exe "E-MU ASIO" 6 7  "E-MU ASIO" 2 3 1000.0 -1 1 1 900 1000 220 1 0 0 24 255 200 0 "mywindowclass" "mywindowtitle" "" ""
+start spiprocesslowpasswin32.exe "E-MU ASIO" 0 1  "E-MU ASIO" 2 3 1000.0 -1 1 1 900 1000 220 1 0 0 24 255 200 0 "mywindowclass" "mywindowtitle" "" ""
